99c42e4f579baff3386f3e2ef542b016c74f47e9
AbdenourN <papikonotoc1@gmail.com>
2024-06-26 03:29:04 +0200

    ADD fichiers git

--- Forum_V2/Views/view_carte.php
+++ Forum_V2/Views/view_carte.php
@@ -8,19 +8,19 @@
     <map name="map">
       
         
-        <area target="" alt="" title="Sujets" href="/Exo/Forum_V2/chrislin/?controller=forum" coords="466,233,400,283" shape="rect">
-        <area target="" alt="" title="" href="/Exo/Forum_V2/chrislin/?controller=forum" coords="" shape="0">
+        <area target="" alt="" title="Sujets" href="/Forum_V2/?controller=forum" coords="466,233,400,283" shape="rect">
+        <area target="" alt="" title="" href="/Forum_V2/?controller=forum" coords="" shape="0">
         
-        <area target="" alt="" title="Galerie Photo" href="/Exo/Forum_V2/chrislin/?controller=forum&action=galerie" coords="390,630,555,820" shape="rect">
-        <area target="" alt="" title="" href="/Exo/Forum_V2/chrislin/?controller=forum" coords="" shape="0">
+        <area target="" alt="" title="Galerie Photo" href="/Forum_V2/?controller=forum&action=galerie" coords="390,630,555,820" shape="rect">
+        <area target="" alt="" title="" href="/Forum_V2/?controller=forum" coords="" shape="0">
 
-        <area target="" alt="" title="Spatialisation" href="/Exo/Forum_V2/chrislin/?controller=forum&action=map" coords="729,506,663,556" shape="rect">
+        <area target="" alt="" title="Spatialisation" href="/Forum_V2/?controller=forum&action=map" coords="729,506,663,556" shape="rect">
         <area target="" alt="" title="" href="" coords="" shape="0"> 
 
-        <area target="" alt="" title="Profil" href="/Exo/Forum_V2/chrislin/?controller=profil" coords="1143,198,1077,248" shape="rect">
+        <area target="" alt="" title="Profil" href="/Forum_V2/?controller=profil" coords="1143,198,1077,248" shape="rect">
         <area target="" alt="" title="" href="" coords="" shape="0">
 
-        <area target="" alt="" title="Message privé" href="/Exo/Forum_V2/chrislin/?controller=mp" coords="1279,593,1252,652" shape="rect">
+        <area target="" alt="" title="Message privé" href="/Forum_V2/?controller=mp" coords="1279,593,1252,652" shape="rect">
         <area target="" alt="" title="" href="" coords="" shape="0"> 
     
     </map>
